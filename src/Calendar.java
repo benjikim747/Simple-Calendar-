29f0f52758064587731717d1b637e0bd147efe57
Dason314 <40226883+Dason314@users.noreply.github.com>
2019-02-22 15:29:54 +0900

    using simple while loop in Calendar

--- src/Calendar.java
+++ src/Calendar.java
@@ -17,22 +17,29 @@ public class Calendar {
 	}
 
 	public static void main(String[] args) {
+
+		String PROMPT = "입력: ";
 		Scanner sc = new Scanner(System.in);
 		Calendar cal = new Calendar();
 
-		System.out.println("반복할 횟수를 입력하세요.");
-		int repeat = sc.nextInt();
+		int month = 1;
+		while (true) { // 무한루프
 
-		for (int i = 0; i < repeat; i++) {
 			System.out.println("달을 입력하세요.");
-			int month = sc.nextInt();
+			System.out.print(PROMPT);
+			month = sc.nextInt();
+			if (month == -1 || month < 1) {
 
-			if (month > 12 || month < 1) {
-				System.out.println("잘못 된 달을 입력하였습니다.");
-			} else {
-				System.out.printf("%d월은 %d일까지 있습니다.\n\n", month, cal.TotalDaysOfaMonth(month));
+				System.out.println("-----------------");
+				break;
 			}
+			if (month > 12) {
+				continue;
+			}
+			System.out.printf("%d월은 %d일까지 있습니다.\n\n", month, cal.TotalDaysOfaMonth(month));
 			System.out.println("-----------------");
 		}
+		System.out.println("캘린더를 종료합니다.");
+		sc.close();
 	}
 }
